4a8a7ab492f090cac7ff0e90021e1ffa875d65fd
MamadTvl <mtavallai80@gmail.com>
2022-07-31 17:27:53 +0430

    add streamModule to userModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { StreamModule } from './stream/stream.module';
 
 @Module({
     controllers: [UserController],
     providers: [UserService],
+    imports: [StreamModule],
 })
 export class UserModule {}
