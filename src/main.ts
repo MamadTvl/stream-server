b6e6e112aa93b1a445fc56159f3deaf617f58661
MamadTvl <mtavallai80@gmail.com>
2022-07-31 17:29:45 +0430

    add mongoose debug log in development mode

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import mongoose from 'mongoose';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(process.env.API_PORT);
+    mongoose.set('debug', process.env.NODE_ENV === 'development');
 }
 bootstrap();
