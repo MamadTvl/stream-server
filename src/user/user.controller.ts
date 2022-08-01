7b713ea2eac5fa85a5f07b28bc04ad58f10fb09f
MamadTvl <mtavallai80@gmail.com>
2022-08-01 14:15:28 +0430

    add method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { AuthRequest } from 'src/common/middleware/authorization.middleware';
+import { Controller, Post, Req, BadRequestException } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @Post('add')
+    addUser(@Req() req: AuthRequest) {
+        throw new BadRequestException('Method not implemented yet');
+    }
 }
